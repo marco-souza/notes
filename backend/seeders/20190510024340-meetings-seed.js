849fa07e6c8d2ee7bedf1db8b80faf59b73111df
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-23 23:48:06 -0300

    ENH: make seed generator

--- backend/seeders/20190510024340-meetings-seed.js
+++ backend/seeders/20190510024340-meetings-seed.js
@@ -1,62 +1,52 @@
 const uuid4 = require('uuid4');
 
+const generateMeetingsAndNotes = () => {
+    const listData = Array(5).fill(true);
+    return listData.map(() => {
+        const meetingId = uuid4();
+        const meeting = {
+            id: meetingId,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            title: 'Standup',
+            startAt: new Date()
+        };
+        const note = {
+            meetingId,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        const notes = Array(5)
+            .fill(true)
+            .map((_, i) => ({
+                ...note,
+                text: `bla ${i}`,
+                id: uuid4()
+            }));
+
+        return {
+            meeting,
+            notes
+        };
+    });
+};
+
 module.exports = {
     up: queryInterface =>
         queryInterface.sequelize.transaction(async transaction => {
-            await queryInterface.bulkInsert(
-                'meeting',
-                [
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Standup',
-                        startAt: new Date()
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Sprint Planning',
-                        startAt: new Date()
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Team Lunch',
-                        startAt: new Date()
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Dinner',
-                        startAt: new Date()
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Dinner',
-                        startAt: new Date('December 15, 1995 03:24:00')
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Dinner',
-                        startAt: new Date('December 17, 1995 03:24:00')
-                    },
-                    {
-                        id: uuid4(),
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        title: 'Dinner',
-                        startAt: new Date('December 18, 1995 03:24:00')
-                    }
-                ],
-                { transaction }
-            );
+            const seeds = generateMeetingsAndNotes();
+
+            const meetings = seeds.map(item => item.meeting);
+            console.log('Seed meetings');
+            await queryInterface.bulkInsert('meeting', meetings, {
+                transaction
+            });
+
+            const notes = seeds.map(item => item.notes).reduce((acc, cur) => [...acc, ...cur], []);
+            console.log('Seed notes');
+            await queryInterface.bulkInsert('note', notes, {
+                transaction
+            });
         })
 };
