12e4414dcd89459a52014638612191f8de6163af
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-23 21:52:57 -0300

    ENH: show notes related to mettings

--- backend/controllers/meeting/meeting.js
+++ backend/controllers/meeting/meeting.js
@@ -6,24 +6,15 @@ const { Meeting, Note } = require('../../models');
  * @returns {Promise<Array<Object>>} The list of meetings.
  */
 async function getMeetings() {
-    const meetings = await Meeting.findAll();
-    return meetings.map(meeting => meeting.get({ plain: true }));
-}
-
-/**
- * List meeting notes.
- * @public
- * @param {object} body - ignore body
- * @param {object} params - request params
- * @returns {Promise<Array<Object>>} List of notes.
- */
-async function listMeetingNotes(body, params) {
-    const note = await Note.findAll({
-        where: {
-            meetingId: params.id
-        }
+    const meetings = await Meeting.findAll({
+        include: [
+            {
+                model: Note,
+                required: true
+            }
+        ]
     });
-    return note;
+    return meetings;
 }
 
 /**
@@ -44,5 +35,4 @@ async function createMeetingNote({ text }, params) {
 module.exports = {
     getMeetings,
     createMeetingNote,
-    listMeetingNotes,
 };
--- backend/routes/meeting/meeting.js
+++ backend/routes/meeting/meeting.js
@@ -2,12 +2,11 @@ const KoaRouter = require('koa-router');
 const {
     wrappers: { http }
 } = require('../../middleware');
-const { getMeetings, createMeetingNote, listMeetingNotes } = require('../../controllers');
+const { getMeetings, createMeetingNote } = require('../../controllers');
 
 const router = KoaRouter();
 
 router.get('/', http(() => getMeetings()));
-router.get('/:id/notes', http(listMeetingNotes));
 router.post('/:id/notes', http(createMeetingNote));
 
 module.exports = router;
