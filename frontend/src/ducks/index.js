04b00e17cd3d43ec8a5be7e769386c86cbe7670e
frattezi <pfrattezi@gmail.com>
2019-05-24 21:46:25 -0300

    ENH: export actions and reducers

--- frontend/src/ducks/index.js
+++ frontend/src/ducks/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux-immutable';
 import meetings from './meeting';
+import notes from './note';
 
 export * from './meeting';
+export * from './note';
 
 export default combineReducers({
-    meetings
+    meetings,
+    notes
 });
