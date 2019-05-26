8cc2d05e889fcca07401aa46ba5458a4a6709105
frattezi <pfrattezi@gmail.com>
2019-05-26 20:58:16 -0300

    FIX: remove selector move

--- frontend/src/ducks/editor.js
+++ frontend/src/ducks/editor.js
@@ -1,42 +1,24 @@
-import { EditorState, ContentState, SelectionState } from 'draft-js';
+import { EditorState, ContentState } from 'draft-js';
 import { createAction, handleActions } from 'redux-actions';
 
 export const setEditorValue = createAction('SET_EDITOR_VALUE');
 export const setEditorState = createAction('SET_EDITOR_STATE');
+export const selectionToEnd = createAction('SELECTION_TO_END');
 
 const INITIAL_STATE = {
     value: EditorState.createEmpty()
 };
 
 const textToEditorState = text => EditorState.createWithContent(ContentState.createFromText(text));
-const moveSelectionToEnd = editorState => {
-    const content = editorState.getCurrentContent();
-    const blockMap = content.getBlockMap();
-
-    const key = blockMap.last().getKey();
-    const length = blockMap.last().getLength();
-
-    // On Chrome and Safari, calling focus on contenteditable focuses the
-    // cursor at the first character. This is something you don't expect when
-    // you're clicking on an input element but not directly on a character.
-    // Put the cursor back where it was before the blur.
-    const selection = new SelectionState({
-        anchorKey: key,
-        anchorOffset: length,
-        focusKey: key,
-        focusOffset: length
-    });
-    return EditorState.forceSelection(editorState, selection);
-};
 
 export default handleActions(
     {
         [setEditorValue]: (state, { payload }) => ({
-            value: moveSelectionToEnd(textToEditorState(payload))
+            value: textToEditorState(payload)
         }),
 
         [setEditorState]: (state, { payload }) => ({
-            value: moveSelectionToEnd(payload)
+            value: payload
         })
     },
     INITIAL_STATE
