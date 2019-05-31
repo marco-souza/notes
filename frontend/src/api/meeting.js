debae2a885612f5a1e9719ab83d20898e5aa8e2a
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-30 23:03:55 -0300

    ENH: implement saga to save note

--- frontend/src/api/meeting.js
+++ frontend/src/api/meeting.js
@@ -1,5 +1,9 @@
-import { get } from '../lib/HTTP';
+import { get, post } from '../lib/HTTP';
 
 export function getMeetingsFromAPI() {
     return get(`/meetings`);
+}
+
+export function upsertNoteForMeetingsFromAPI(id, body) {
+    return post(`/meetings/${id}/notes`, null, body);
 }
