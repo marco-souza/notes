28f598a4c2ee45c45cd2d056cffc70ade59391ce
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-31 16:15:49 -0300

    FEA: add notes list
    - Add item
    - List items
    - Container to show lists

---
+++ frontend/src/components/NoteListItem.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { ListItem } from 'material-ui';
+import { Note } from '../models';
+
+const styles = {
+    selected: {
+        background: 'rgba(0, 0, 0, 0.15)'
+    }
+};
+
+export default class NoteListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            primaryText: this.parseDateToLocalTimezone(props.note.createdAt)
+        };
+    }
+
+    parseDateToLocalTimezone(date) {
+        const TZ_OFFSET = date.getTimezoneOffset() * 60 * 1000;
+        date.setTime(date.getTime() + TZ_OFFSET);
+        return moment(date.toISOString()).format('MMMM Do YY, h:mm a');
+    }
+
+    handleClick() {
+        const { note, onClick } = this.props;
+        onClick(note.id);
+    }
+
+    stripHTML(html) {
+        const tmp = document.createElement('div');
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || '';
+    }
+
+    render() {
+        const { note, isSelected } = this.props;
+
+        return (
+            <div style={isSelected ? styles.selected : null}>
+                <ListItem
+                    onClick={this.handleClick}
+                    primaryText={this.state.primaryText}
+                    secondaryText={this.stripHTML(note.text)}
+                />
+            </div>
+        );
+    }
+}
+
+NoteListItem.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    note: PropTypes.instanceOf(Note).isRequired,
+    isSelected: PropTypes.bool
+};
+
+NoteListItem.defaultProps = {
+    isSelected: false
+};
---
+++ frontend/src/containers/NotesList/NotesList.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NoteListItem, NoteListAddItem, SideBar } from '../../components';
+import { Editor } from '../index';
+import { setEditingNote, setEditorValue } from '../../ducks';
+import { getNotes } from '../../selectors';
+
+function NotesList(props) {
+    const { notes, onSetEditingNote, onSetEditorValue } = props;
+    const [selected, setSelected] = useState(null);
+    const handleClick = ({ id, text }) => {
+        onSetEditorValue(text);
+        onSetEditingNote(id);
+        setSelected(id);
+    };
+    const handleAddClick = () => {
+        onSetEditorValue('');
+        onSetEditingNote(true);
+        setSelected(null);
+    };
+
+    return !notes.isVisible ? null : (
+        <SideBar title="Notes" rightSide={notes.editingNote && <Editor />}>
+            <NoteListAddItem addNewItem={handleAddClick} />
+
+            {notes.notes &&
+                notes.notes.map(note => (
+                    <NoteListItem
+                        isSelected={selected === note.id}
+                        key={note.id}
+                        note={note}
+                        onClick={() => handleClick(note)}
+                    />
+                ))}
+        </SideBar>
+    );
+}
+
+NotesList.propTypes = {
+    notes: PropTypes.object.isRequired,
+    onSetEditingNote: PropTypes.func.isRequired,
+    onSetEditorValue: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        notes: getNotes(state)
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        // Notes Actions
+        onSetEditingNote: id => dispatch(setEditingNote(id)),
+
+        // Editor Actions
+        onSetEditorValue: text => dispatch(setEditorValue(text))
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(NotesList);
