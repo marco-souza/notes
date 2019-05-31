1e83eab720138c0b9bb8d67ae3e80abfa8c6288d
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-30 23:47:28 -0300

    ENH: allow edit HTML content

--- frontend/src/ducks/editor.js
+++ frontend/src/ducks/editor.js
@@ -1,4 +1,4 @@
-import { EditorState, ContentState } from 'draft-js';
+import { EditorState, ContentState, convertFromHTML } from 'draft-js';
 import { createAction, handleActions } from 'redux-actions';
 
 export const setEditorValue = createAction('SET_EDITOR_VALUE');
@@ -6,16 +6,23 @@ export const setEditorState = createAction('SET_EDITOR_STATE');
 export const selectionToEnd = createAction('SELECTION_TO_END');
 
 const INITIAL_STATE = {
-    meetingId: null,
     value: EditorState.createEmpty()
 };
 
 const textToEditorState = text => EditorState.createWithContent(ContentState.createFromText(text));
+const htmlToEditorState = text => {
+    const blocksFromHTML = convertFromHTML(text);
+    const content = ContentState.createFromBlockArray(
+        blocksFromHTML.contentBlocks,
+        blocksFromHTML.entityMap
+    );
+    return EditorState.createWithContent(content);
+};
 
 export default handleActions(
     {
         [setEditorValue]: (state, { payload }) => ({
-            value: textToEditorState(payload)
+            value: payload[0] === '<' ? htmlToEditorState(payload) : textToEditorState(payload)
         }),
 
         [setEditorState]: (state, { payload }) => ({
