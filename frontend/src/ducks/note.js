325578a2906a8ea6fb85ff13020715aa15b8a633
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-30 23:04:49 -0300

    REF: pass meetingId to save data

--- frontend/src/ducks/note.js
+++ frontend/src/ducks/note.js
@@ -6,9 +6,13 @@ export const setNotesVisibility = createAction('SET_NOTES_VISIBILITY');
 export const setNotes = createAction('SET_NOTES');
 export const setEditingNote = createAction('SET_EDITING_NOTE');
 
+export const saveNote = createAction('SAVE_NOTE');
+export const saveNoteCompleted = createAction('SAVE_NOTE_COMPLETED');
+
 const INITIAL_STATE = {
     isVisible: false,
     notes: Immutable.List(),
+    meetingId: null,
     editingNote: null
 };
 
@@ -22,12 +26,22 @@ export default handleActions(
 
         [setNotes]: (state, { payload }) => ({
             ...state,
+            meetingId: payload.id,
             notes: Immutable.List(payload.notes.map(note => new Note(note)))
         }),
 
         [setEditingNote]: (state, { payload }) => ({
             ...state,
             editingNote: payload
+        }),
+
+        [saveNoteCompleted]: (state, { payload }) => ({
+            ...state,
+            notes: Immutable.List([
+                new Note(payload.note),
+                ...state.notes.map(note => new Note(note))
+            ]),
+            editingNote: payload.note.id
         })
     },
     INITIAL_STATE
