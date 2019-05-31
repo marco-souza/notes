5d945536a68e848f618a3e44b50ee5ed5258a73d
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-31 16:23:04 -0300

    ENH: edit MeetingListItem
    - add parsed date to local tz
    - select item

--- frontend/src/components/MeetingListItem.js
+++ frontend/src/components/MeetingListItem.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { ListItem } from 'material-ui';
 import { Meeting } from '../models';
+
+const styles = {
+    selected: {
+        background: 'rgba(0, 0, 0, 0.15)'
+    }
+};
 
 export default class MeetingListItem extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            secondaryText: this.parseDateToLocalTimezone(props.meeting.startAt)
+        };
+    }
+
+    parseDateToLocalTimezone(date) {
+        const TZ_OFFSET = date.getTimezoneOffset() * 60 * 1000;
+        date.setTime(date.getTime() + TZ_OFFSET);
+        return moment(date.toISOString()).fromNow();
     }
 
     handleClick() {
@@ -15,19 +31,21 @@ export default class MeetingListItem extends Component {
     }
 
     render() {
-        const { meeting } = this.props;
-
+        const { meeting, isSelected } = this.props;
         return (
-            <ListItem
-                onClick={this.handleClick}
-                primaryText={meeting.title}
-                secondaryText={meeting.startAt.toISOString()}
-            />
+            <div style={isSelected ? styles.selected : null}>
+                <ListItem
+                    onClick={this.handleClick}
+                    primaryText={meeting.title}
+                    secondaryText={this.state.secondaryText}
+                />
+            </div>
         );
     }
 }
 
 MeetingListItem.propTypes = {
+    isSelected: PropTypes.bool.isRequired,
     onClick: PropTypes.func.isRequired,
     meeting: PropTypes.instanceOf(Meeting).isRequired
 };
